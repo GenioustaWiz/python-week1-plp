69db9361ffae0a887f05d03428466354a9f76145
GenioustaWiz <cm0215557@gmail.com>
2024-08-26 18:28:47 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,10 @@
+
+print("Hello, please pay attention to the prompts and enter the information requested. Thank you")
+
+name = input("Please enter your first name: ")
+
+age = input("Please enter your age: ")
+
+location = input("Where is your home located at?: ")
+
+print(f"Your name is {name}, your are {age} old, and you live at {location}")
